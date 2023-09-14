f93ab951b41a62eb2d2d16c589066fca0295167c
Peter Steinberg <bklynpeter@gmail.com>
2023-09-14 18:36:00 -0400

    lesson 1: initial deconstruction

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const element = <h1 title="web dev made simple">DevSage</h1>;
-const root = document.getElementById("root")
-ReactDOM.render(element, root);
+// const element = <h1 title="web dev made simple">DevSage</h1>;
+// const element = React.createElement(
+//   'h1',
+//   { title: 'web dev made simple' },
+//   'DevSage'
+// );
+const element = {
+  type: 'h1',
+  props: {
+    title: 'web dev made simple',
+    children: ['DevSage'],
+  },
+};
+
+// const root = document.getElementById('root');
+const container = document.getElementById('root');
+
+const node = document.createElement(element.type);
+node['title'] = element.props.title;
+
+const text = document.createTextNode('');
+text['nodeValue'] = element.props.children;
+
+node.appendChild(text);
+container.appendChild(node);
+
+// ReactDOM.render(element, root);
